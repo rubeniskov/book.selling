20e9f7f4a7577fa2c53fbc84a5d7942798cbf3d0
jal88 <jal1988j@gmail.com>
2014-08-27 20:04:02 +0200

    SITE: Modulos myaccount, header y footer actualizados

---
+++ modules/site/my-account-uploaded-books/index.js
@@ -0,0 +1,17 @@
+module.exports = function( $ )
+{
+	return ({
+
+		__render : function()
+		{
+			var query 	= $.mysql.query( "SELECT * FROM tb_books" );
+
+			if( query.error )
+			{
+				return false;
+			}
+
+			return { books : query.result };
+		}
+	})
+};
