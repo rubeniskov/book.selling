a607b2ce191a6ff45ba529699813d807652a9e51
jal88 <jal1988j@gmail.com>
2014-08-31 23:46:33 +0200

    SITE:Modulos falta de myacount-books-purchased

--- services/site/modules/my-account-books/index.js
+++ services/site/modules/my-account-books/index.js
@@ -2,7 +2,9 @@ module.exports = function($) {
     return ({
 
         __render: function() {
-            var query = $.mysql.query("SELECT * FROM tb_books");
+            var query = $.mysql.query("SELECT * FROM db_bookselling.v_books_uploaded;");
+
+
 
             if (query.error) {
                 return false;
