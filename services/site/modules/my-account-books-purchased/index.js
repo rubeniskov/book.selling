4b4dd5730b552ea1b399178ee79b12ca91be620c
jal88 <jal1988j@gmail.com>
2014-09-01 13:31:02 +0200

    SITE:MODULOS. Falta modulo detalles del libro

--- services/site/modules/my-account-books-purchased/index.js
+++ services/site/modules/my-account-books-purchased/index.js
@@ -4,14 +4,12 @@ module.exports = function($) {
         __render: function() {
             var i, purchases,
 
-                query = $.mysql.query("SELECT *, DATE_FORMAT( purchase_date, '%d-%m-%Y') AS purchase_date_formatted FROM db_bookselling.tb_purchases");
+            query = $.mysql.query("SELECT *, DATE_FORMAT( purchase_date, '%d-%m-%Y') AS purchase_date_formatted FROM db_bookselling.tb_purchases");
             //var query2 	= $.mysql.query( "SELECT * FROM db_bookselling.tb_books_purchased;" );
 
             if (query.error) {
                 return false;
-            }
-
-            ;
+            };
 
             for (i = 0, purchases = query.result; i < purchases.length; i++) {
                 query = $.mysql.query("SELECT * FROM db_bookselling.v_books_purchased WHERE purchase_id = :purchase_id;", purchases[i])
