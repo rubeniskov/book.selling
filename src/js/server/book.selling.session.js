926b012a02528017acad0a8d728e3e39f13189f0
ruben.lopez <loslopez1@icloud.com>
2014-09-04 01:01:12 +0200

    Add: Archivos session para mongodb

---
+++ src/js/server/book.selling.session.js
@@ -0,0 +1,18 @@
+(function( bs ){
+    
+    var swig        = require('mongodb'),
+
+        cheerio     = require("cheerio"),
+
+        fs          = require("fs"),
+
+        path        = require("path");
+
+    bs.session      = function( res, req, next )
+    {
+        
+    }
+
+    bs.session.store= {};
+    
+})( BookSelling );
