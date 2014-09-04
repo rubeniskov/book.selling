a064a1be59d4f6037dc0694d52b5299238195631
jal88 <jal1988j@gmail.com>
2014-09-04 16:07:29 +0200

    SITE: Fuentes cargadas,

--- src/db/querys.sql
+++ src/db/querys.sql
@@ -4,3 +4,14 @@ SELECT * FROM db_bookselling.tb_books_uploaded;
 SELECT * FROM db_bookselling.tb_books_purchased;
 -- Mostrar Libros Disponibles
 SELECT * FROM db_bookselling.v_books_available;
+
+--BOTON ORDENAR Afabetico A-Z--
+SELECT * FROM db_bookselling.tb_books ORDER BY book_title;
+
+--BOTON ORDENAR Afabetico Z-A--
+SELECT * FROM db_bookselling.tb_books ORDER BY book_title DESC;
+
+--BOTON ORDENAR Precio A-Z--
+SELECT * FROM db_bookselling.tb_books ORDER BY book_price;
+--BOTON ORDENAR Precio A-Z--
+SELECT * FROM db_bookselling.tb_books ORDER BY book_price DESC;
--- src/js/server/book.selling.login.js
+++ src/js/server/book.selling.login.js
@@ -7,7 +7,7 @@
             var query = bs.mysql.query
             ( 
                 "INSERT INTO `tb_users`( `user_email`, `user_password`, `user_name`, `user_surname`, `user_birthdate`, `user_sex`, `user_phone`, `user_address`, `user_cp`, `user_state`, `user_country`) " +
-                "VALUES( :user_email, :user_password, :user_name, :user_surname, :user_birthdate, :user_sex, :user_phone, :user_address, :user_cp, :user_state, :user_country )", 
+                "VALUES( :user_email,  MD5(:user_password), :user_name, :user_surname, :user_birthdate, :user_sex, :user_phone, :user_address, :user_cp, :user_state, :user_country )", 
                 data
             );
         },
