3adbd483bf819733f3131b9b233c48be119673d8
jal88 <jal1988j@gmail.com>
2014-09-06 21:24:34 +0200

    SITE:Ordenar
    Conflicts:
    	package.json

--- services/site/modules/books-list/index.js
+++ services/site/modules/books-list/index.js
@@ -1,9 +1,13 @@
 module.exports = function($) {
     return ({
 
-        __render: function() {
-            //var query = $.mysql.query("SELECT * FROM db_bookselling.v_books;");
-            var query = $.mysql.query("SELECT * FROM db_bookselling.v_books_available;");
+        __render: function( app ) {
+            
+            var sort    = app.params.sort || 'book_title|desc',
+
+                sort    = sort.split( '|' );
+
+            var query   = $.mysql.query('SELECT * FROM db_bookselling.v_books_available ORDER BY '+ sort[ 0 ] +' ' + sort[ 1 ] + '; ');
 
 
 
@@ -14,6 +18,15 @@ module.exports = function($) {
             return {
                 books: query.result
             };
+        },
+        __ready: function($){
+
+            var form = $("form[name=book-order-form]");
+
+            form.find( 'select' ).change(function(e){ 
+
+                form.submit();
+            })   
         }
     })
 };
