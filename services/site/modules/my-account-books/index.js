a6ddee1f59a4b27a4300d48ecb49a8997ba0ce2d
jal88 <jal1988j@gmail.com>
2014-09-06 18:02:59 +0200

    SITE: Consulta mis libros subidos por user

--- services/site/modules/my-account-books/index.js
+++ services/site/modules/my-account-books/index.js
@@ -1,8 +1,11 @@
 module.exports = function($) {
     return ({
 
-        __render: function() {
-            var query = $.mysql.query("SELECT * FROM db_bookselling.v_books_uploaded;");
+        __render: function(app) {
+            var query = $.mysql.query("SELECT * FROM db_bookselling.v_books_uploaded where user_id=:user_id",
+                { 
+                    user_id : app.user.user_id 
+                });
 
 
 
