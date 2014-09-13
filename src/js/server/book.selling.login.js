20deb46b5160ee0cdf1cbf6fd2a2a62fac91a288
ruben.lopez <loslopez1@icloud.com>
2014-09-13 22:44:00 +0200

    Fix: en login y eliminado logout

--- src/js/server/book.selling.login.js
+++ src/js/server/book.selling.login.js
@@ -10,19 +10,15 @@
                 "VALUES( :user_email,  MD5(:user_password), :user_name, :user_surname, :user_birthdate, :user_sex, :user_phone, :user_address, :user_cp, :user_state, :user_country )", 
                 data
             );
+
+            console.log( query );
         },
         signIn   : function( credentials )
         {
-            var query = bs.mysql.query( "SELECT user_id, user_name FROM tb_users WHERE user_email=:user_email AND user_password=:user_password", credentials );
+            var query   = bs.mysql.query( "SELECT user_email, user_name, user_surname, user_birthdate, user_sex, user_phone, user_address, user_cp, user_state, user_country FROM tb_users WHERE user_email=:user_email AND user_password=:user_password", credentials );
 
-
-            return query.result[0];
-        },
-        signOut: function() 
-        {
-
+            return query.error ? false : query.result[ 0 ];
         }
-            
-        });
+    });
 
 })(BookSelling);
