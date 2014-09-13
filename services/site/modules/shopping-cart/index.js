ba36b7e0e91e3e4e5f420f71ee6e2a9bce751aed
ruben.lopez <loslopez1@icloud.com>
2014-09-13 22:44:45 +0200

    Add: Implementado carrito de compra

--- services/site/modules/shopping-cart/index.js
+++ services/site/modules/shopping-cart/index.js
@@ -1,60 +1,129 @@
 module.exports = function( bs )
 {
-	var cart_items = 
-	[
-		{book_title:'La biblia', user_name: 'Jorge Alonso',book_uploaded_price: 21.5},
-		{book_title:'La biblia2', user_name: 'Jorge Alonso',book_uploaded_price: 24.5},
-		{book_title:'La biblia3', user_name: 'Jorge Alonso',book_uploaded_price: 11.5}
-	];
+	var und 		= require( 'underscore' ),
+
+		getTotal 	= function( items )
+		{
+			var total = 0;
+			
+			und.each( items, function( item, ref )
+			{
+				total += item.book_uploaded_price;
+			});
+
+			return total;
+		},
+		getCount 	= function( items )
+		{
+			var count = 0;
+
+			und.each( items, function( item, ref )
+			{
+				count ++;
+			});
+
+			return count;
+		}
 
 	return ({
-		__render : function()
-		{
-			return ({
-				items: cart_items
-			});
-		},
 		__ready : function( $ )
 		{
+
 			$( '[add-to-cart-book]', document.body ).click(function(e)
 			{
-				$.socket.emit('add',$(this).attr('add-to-cart-book'));
+				var btn 	= $(this);
 
-				$.socket.on('added', function()
+				$.socket.emit( 'add', btn.attr( 'add-to-cart-book' ) );
+
+				btn
+					.unbind( 'click' )
+					.css( 'background-color', '#efefef' )
+					.text( 'Añadido' );
+			});
+
+			$.socket.on('change', function( books, count, total )
+			{
+				var table 	= $( 'table > tbody' ).empty();
+
+				$.each( books, function( ref, book )
 				{
-					$( 'table > tbody' ).append
+					table.append
 					(
 						'<tr>'+
 				            '<td>'+
-				                '<a href="/book/{{item.book_isbn}}">{{item.book_title}}</a>'+
+				                '<a href="/book/' + book.book_isbn + '">' + book.book_title + '</a>'+
 				            '</td>'+
 				            '<td>'+
-				                '{{item.user_name}}'+
+				                book.user_name+
 				            '</td>'+
 				            '<td>'+
-				                '{{item.book_uploaded_price}}'+
+				                book.book_uploaded_price+
+				            '</td>'+
+				            '<td>'+
+				                '<a class="btn-sm btn-danger" remove-from-cart-book="'+ref+'"><i class="fa fa-minus"></i></a>'+
 				            '</td>'+
 				        '</tr>'
 					);
 				});
+
+				$( '[remove-from-cart-book]' ).click( function()
+				{
+					var btn = $( this );
+
+					btn.closest( 'tr' ).hide( 1000, function()
+					{
+						$.socket.emit( 'remove', btn.attr( 'remove-from-cart-book' ) );	
+					})
+				});	
+
+				$( '[data-shopping-cart-total]', document.body ).text( total );
+
+				$( '[data-shopping-cart-count]', document.body ).text( count );
 			});
+
+			$.socket.emit( 'refresh' );
+
 		},
 		events : 
 		({
 
-			'add': function( book_uploaded_id )
+			'add' 		: function( book_uploaded_id )
 			{
-				console.log(book_uploaded_id)
+				var items 	= this.session.get( 'shopping-cart' ) || {},
 
-				var query = bs.mysql.query
+					query 	= bs.mysql.query
 				(
-					'SELECT book_uploaded_id, book_uploaded_price, book_title, user_name FROM db_bookselling.v_books_uploaded ' +
+					'SELECT book_isbn, book_uploaded_id, book_uploaded_price, book_title, user_name FROM db_bookselling.v_books_uploaded ' +
 					'LEFT JOIN db_bookselling.tb_users USING (user_id) WHERE book_uploaded_id = ' + book_uploaded_id
-				);
+				),
+					book 	= query.error || query.result[ 0 ];
 
-				cart_items[book_uploaded_id] = query.result;
+				if( book )
+				{
+					items[ book.book_isbn + '-' + book.book_uploaded_id ] = book;
 
-				this.emit( 'module-shopping-cart.added', cart_items );
+					this.session.set( 'shopping-cart', items );
+
+					this.emit( 'change', items, getCount( items ), getTotal( items ) );
+				}
+			},
+			'remove'  	: function( book_ref )
+			{
+				var items 	= this.session.get( 'shopping-cart' ) || {};
+
+				delete items[ book_ref ];
+
+				this.session.set( 'shopping-cart', items );
+
+				this.emit( 'change', items, getCount( items ), getTotal( items ) );
+			},
+			'refresh' 	: function()
+			{
+				var items 	= this.session.get( 'shopping-cart' ) || {};
+
+				this.session.set( 'shopping-cart', items );
+
+				this.emit( 'change', items, getCount( items ), getTotal( items ) );
 			}
 		})
 	})
